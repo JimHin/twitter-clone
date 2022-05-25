d78f20591e7e9813491f25b9da355480cb0fab13
JimKrow <krowstudio@icloud.com>
2022-05-25 16:14:19 +0200

    déclaration du formulaire

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,23 +1,18 @@
 function Form() {
     return (
-            <form action="action_page.php">
-                    <h1>Register</h1>
-                    <p>Please fill in this form to create an account.</p>
-                    <hr/>
-                    <label htmlFor="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" required />
-
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
-
-                    <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat"
-                           required />
-                        <hr/>
-
-                    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-                    <button type="submit" className="registerbtn">Register</button>
+        <div className="formulaire">
+            <form action="#">
+                <br/>
+                <label htmlFor="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required />
+                <br/>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
+                <br/>
+                <button type="submit" className="registerbtn">Login</button>
             </form>
+        </div>
+
 );
 }
 
